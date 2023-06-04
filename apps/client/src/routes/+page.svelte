216240b5f2a2cb75eec8e2e966382845688553d3
Daisho Komiyama <daishokomiyama@gmail.com>
2023-06-04 15:31:13 -0400

    Add a bare minimum home page

---
+++ apps/client/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<div class="home">
+	<h1>Frontend Community</h1>
+
+	<div class="links">
+		<a href="/upload">Upload files</a>
+		<a href="/todo">TODO</a>
+	</div>
+</div>
+
+<style>
+	.home {
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+	}
+
+	.links {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+</style>
