c48dc4c97651412cea74edb3d7ab2390441882c6
Daisho Komiyama <daishokomiyama@gmail.com>
2023-08-28 21:23:05 -0400

    Organize around recaptcha

---
+++ apps/client/src/lib/RecaptchaPrivacyPolicy.svelte
@@ -0,0 +1,16 @@
+<p>
+    This site is protected by reCAPTCHA and the Google
+    <a href="https://policies.google.com/privacy">Privacy Policy</a> and
+    <a href="https://policies.google.com/terms">Terms of Service</a> apply.
+</p>
+
+<style>
+    p {
+        background: var(--bg-info);
+        border-radius: var(--border-radius-large);
+        color: #555;
+        display: inline-block;
+        font-size: var(--font-small);
+        padding: 0.75rem 1rem;
+    }
+</style>
