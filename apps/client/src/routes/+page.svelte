84caff8abc4c007aa6dad0f7f062572b44a092ba
Daisho Komiyama <daishokomiyama@gmail.com>
2024-01-07 20:47:31 -0500

    Update the message

--- apps/client/src/routes/+page.svelte
+++ apps/client/src/routes/+page.svelte
@@ -7,12 +7,14 @@
 <h2>Updates - {data.userName} {new Date().getTimezoneOffset()}</h2>
 
 <!-- svelte-ignore a11y-distracting-elements -->
-<marquee class="msg"
-    >There's something about the quiet before the world awakens that clarifies one's purpose. The
+<marquee class="msg" scrollamount="40">
+    There's something about the quiet before the world awakens that clarifies one's purpose. The
     song of the first bird, not yet drowned out by the day's chaos, reminds us of the clarity that
     comes with being the first to act. Sometimes we wonder, are we all just waiting for a new
-    sunrise, or are we the ones who bring it forth?<span>React has to die.</span></marquee
->
+    sunrise, or are we the ones who bring it forth?<span
+        >&block;&block;&block;&block; &block;&block;&block; &block;&block; &block;&block;&block;.</span
+    >
+</marquee>
 
 <style>
     .msg {
