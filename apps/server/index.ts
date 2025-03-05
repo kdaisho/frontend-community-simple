c4da32c4e9ca94c51f2c2059b2fb969ce43a4763
Daisho Komiyama <daishokomiyama@gmail.com>
2025-03-04 20:28:34 -0500

    Don't take a random PORT

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -8,8 +8,6 @@ import cors from 'cors'
 import express from 'express'
 import { appRouter } from './src/services'
 
-const port = process.env.PORT || 3001;
-
 async function server() {
     const app = express()
     app.use(cors())
@@ -39,8 +37,8 @@ async function server() {
         })
     )
 
-    app.listen(port, () => {
-        console.info(`Server: listening on port ${port}`)
+    app.listen(3001, () => {
+        console.info('Server: listening on port 3001')
     })
 }
 
